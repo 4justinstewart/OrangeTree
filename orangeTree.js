20dbfbeb6e56fb7d4bde64e04571f83199d2c74e
Kevin Solorio <soloriok@gmail.com>
2013-07-31 17:04:03 -0500

    one way to solve it...

--- orangeTree.js
+++ orangeTree.js
@@ -1,5 +1,33 @@
 //////// Your Code ///////////////////////////////////////////////
+function OrangeTree(){
+  this.oranges = [];
+  this.age = 0;
+  this.height = 0;
+};
 
+OrangeTree.prototype = {
+  hasOranges: function(){
+    return this.oranges.length > 0;
+  },
+
+  incrementAge: function(){
+    if (this.age > 3) {
+      for(var i = 0; i < 3; i++) {
+        this.oranges.push({diameter: Math.floor(Math.random()*4 + 1)});
+      }
+    }
+    this.height += 12;
+    this.age++;
+  },
+
+  isAlive: function(){
+    return this.age <= 100;
+  },
+
+  pickOrange: function(){
+    return this.oranges.pop();
+  }
+};
 
 
 /////// Driver Code /////////////////////////////////////////////
@@ -10,7 +38,7 @@ while (!tree.hasOranges()) {
   tree.incrementAge();
 }
 
-console.log("Tree is " + tree.age + " years old and " + tree.height + " feet tall");
+console.log("Tree is " + tree.age + " years old and " + tree.height/12 + " feet tall");
 
 while(tree.isAlive()) {
   var basket = [];
@@ -21,11 +49,14 @@ while(tree.isAlive()) {
 
   var avgDiameter = function() {
     // create this method
+    return basket.reduce(function(previous, current){
+      return previous + current.diameter;
+    }, 0)/basket.length;
   };
 
   console.log("Year " + tree.age + " Report");
-  console.log("Tree height: " + tree.height + " feet");
-  console.log("Harvest: " + basket.length + " oranges with an average diameter of " + avgDiameter() + " inces");
+  console.log("Tree height: " + tree.height/12 + " feet");
+  console.log("Harvest: " + basket.length + " oranges with an average diameter of " + avgDiameter() + " inches");
   console.log("");
 
   tree.incrementAge();
